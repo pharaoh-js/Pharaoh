368c0b940da94182062ba36f45ce5681a169ea1c
Adam Romines <adam.romines@gmail.com>
2016-01-07 18:58:31 -0700

    Adds Mirror compenent with firepad instance

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React, { PropTypes } from 'react'
+import Viewer from './viewer/Viewer'
 
 const App = React.createClass({
   render () {
     return (
-        <div><h2>howdy</h2>
-        <small>world</small>
+        <div>
+          <h2>Pharaoh, king of the tutorial tools</h2>
+          <Viewer />
         </div>
     )
   }
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,8 @@
-import React    from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
-import Viewer   from './components/viewer/Viewer'
-import Menu     from './components/common/header/Menu'
+import App from './components/App'
+import Menu from './components/common/header/Menu'
 
 require('./shared/styles.less')
 
 ReactDOM.render(<App />, document.getElementById('root'))
-
