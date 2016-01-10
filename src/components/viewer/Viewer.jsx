e67c54c47c2562b1e9166c704a4568603a3c6008
Adam Romines <adam.romines@gmail.com>
2016-01-09 18:46:45 -0700

    Adds swapDoc function

--- src/components/viewer/Viewer.jsx
+++ src/components/viewer/Viewer.jsx
@@ -2,15 +2,21 @@ import React, { PropTypes } from 'react'
 import Mirror from './mirror/Mirror'
 
 const Viewer = React.createClass({
+  swapDoc (pad) {
+    this.setState({pad:pad})
+  },
   getInitialState () {
     return {
-      pad: '/test3'
+      pad: 'test3'
     }
   },
   render () {
     return (
       <div>
         <h3>This is the viewer component and it is awesome</h3>
+        <button onClick={this.swapDoc.bind(null, 'one')}>one</button>
+        <button onClick={this.swapDoc.bind(null, 'two')}>two</button>
+        <button onClick={this.swapDoc.bind(null, 'three')}>three</button>
         <Mirror pad={this.state.pad} />
       </div>
     )
