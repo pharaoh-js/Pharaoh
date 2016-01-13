d9d91a43d8ab13cac80909af3b42a97f55cec23e
Adam Romines <adam.romines@gmail.com>
2016-01-13 12:13:07 -0700

    Fixes syntax error in index.jsx and cleans up node modules

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 // import ReactRouter from 'react-router'
 import App from './components/app/App'
-// import Settings from './components/settings/Settings'
-// import Team from './components/team/Team'
-// import Howto from './components/howto/Howto'
-// import { Router, Route, Redirect } from 'react-router';
 require('./shared/styles.less')
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 // import createHistory from 'history/lib/createHashHistory';
 // var history = createHistory({
 //   queryKey: false
@@ -16,10 +12,7 @@ ReactDom.render(<App />, document.getElementById('root'));
 // ReactDOM.render(
 //     (
 //     <Router history={history}>
-//     <Redirect from="/" to="/home"/>
-//     <Route path="/home" component={App}/>
-//     <Route path="/settings" component={Settings}/>
-//     <Route path="/team" component={Team}/>
-//     <Route path="/howto" component={Howto}/>
+//     <Route path="/app" component={App}/>
+//     <Route path="/" component={Landing}/>
 //   </Router>
 // ), document.getElementById('root'));
