b4d0bca06923726e76a66b560a4b733bbf21e117
J. Tanner Porter <jtporter9@gmail.com>
2016-01-19 23:07:57 -0700

    more styling to getting started

--- src/components/landing/gettingstarted/GettingStarted.jsx
+++ src/components/landing/gettingstarted/GettingStarted.jsx
@@ -9,36 +9,30 @@ const GettingStarted = React.createClass({
         <div className="container">
           <div className="header">Getting Started</div>
           <div className="option1">
-            <big>Option 1</big>
-            <li>Paraoh browser based editor.</li>
+            <big>Start session from browser editor.
+
+            </big>
             <img src="src/shared/images/pharaohclient.png" style={{width:'47%',float:'left',margin:'20px 0 0 5px'}}></img>
             <div className="start-session">
-              <span>Start new Session
+              <big>Start new Session
                 <input style={{width:'60%'}} placeholder="url extension...." type="text"></input>  <button>Go</button>
-              </span>
+              </big>
               <p>lskdj falksdjfals kdjf;alk dsjf alk d alsk df jlkj lsdkjfa lsk djf alskdjf
               al sk jflsk dfja l sdj fa;ls kdj fa;lk jdf als dkj fa;l skdjf.
               </p>
             </div>
           </div>
           <div className="desktop">
-            <big>Option 2</big>
-            <li>Paraoh Desktop App.</li>
+            <big>Or install Pharaoh desktop app.</big>
             <div>
-              <big className="npmInstall">npm i -g pharaoh</big>
+              <span className="npmInstall">npm i -g pharaoh</span>
               <p>lskdj falksdjfals kdjf;alk dsjf alk d alsk df jlkj lsdkjfa lsk djf alskdjf
               al sk jflsk dfja l sdj fa;ls kdj fa;lk jdf als dkj fa;l skdjf.
               </p>
               <img src="src/shared/images/desktop.png" style={{
-                width:'46%',position:'absolute',right:'8px',bottom:'20px'
+                width:'46%',position:'absolute',right:'8px',bottom:'5px'
                 }}></img>
             </div>
-          </div>
-        </div>
-        <div className="scroll-down">
-          <big>Scroll Down</big>
-          <div>
-            <span style={{position:'relative',left:'2.3px'}}>\</span><span style={{position:'relative',right:'2.3px'}}>/</span>
           </div>
         </div>
       </InlineCss>
@@ -47,3 +41,13 @@ const GettingStarted = React.createClass({
 })
 
 export default GettingStarted
+// <li>Paraoh browser based editor.</li>
+// <img src="http://www.iconsdb.com/icons/preview/white/down-xxl.png"
+//   style={{width:'15px',position:'relative',top:'5px',left:'7px'}}
+// ></img>
+// <div className="scroll-down">
+//   <big>Scroll Down</big>
+//   <div>
+//     <span style={{position:'relative',left:'2.3px'}}>\</span><span style={{position:'relative',right:'2.3px'}}>/</span>
+//   </div>
+// </div>
--- src/components/landing/howto/HowTo.jsx
+++ src/components/landing/howto/HowTo.jsx
@@ -13,7 +13,7 @@ const HowTo = React.createClass({
           <br/>
           <big>About:</big>
           <p>
-            We're a group of students at <a href="http://devmounta.in">DevMountain</a>, building just about the neatest instructor tool you've ever seen. Imagine a lecture with no JSBin or JSFiddle, with no opening the Developer Tools in your browser just to show something in the console, with no rushed projects thrown together and then sent out on Slack. Imagine a read-only app that would allow students to see your code in realtime, as you make changes, and see everything you `import` or `require`, and check out the files you're pulling from.
+            We're a group of students at <a className="devmtn" href="http://devmounta.in">DevMountain</a>, building just about the neatest instructor tool you've ever seen. Imagine a lecture with no JSBin or JSFiddle, with no opening the Developer Tools in your browser just to show something in the console, with no rushed projects thrown together and then sent out on Slack. Imagine a read-only app that would allow students to see your code in realtime, as you make changes, and see everything you `import` or `require`, and check out the files you're pulling from.
               </p><p>Well, that's what we imagined, because right now there's a ton of tooling around Javascript development, but not nearly enough tools for teaching it. We're starting with a React-based student app and a package for one of the most popular editors on the market. We hope to build this into something pretty awesome, because no one should have to use JSBin, Github, and Slack, just to get through one lecture.
           </p>
           <big>Usage:</big>
