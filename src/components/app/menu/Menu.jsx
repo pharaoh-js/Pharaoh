d9ef28f4f18c6ced219fcca67f782ff9e8ebe538
J. Tanner Porter <jtporter9@gmail.com>
2016-01-21 01:56:20 -0700

    menu bar in readitor view

--- src/components/app/menu/Menu.jsx
+++ src/components/app/menu/Menu.jsx
@@ -17,12 +17,18 @@ const Header = React.createClass({
   render () {
     return (
       <InlineCss componentName="Header" stylesheet={stylesheet}>
-          <div className="header">
-            <div>
-              <Link className="title" to={"/"}>PHARAOH</Link>
+        <div className="header">
+          <div>
+            <Link className="title" to={"/"}>Pharaoh</Link>
+            <img src="src/shared/images/pharaoh.png" style={{
+              width: '50px',
+              position: 'absolute',
+              top: '2px',
+              left:'12.5%'
+              }}></img>
                <div className={this.state.invite}>
                 <div onClick={this.toggleCopying}>
-                  <img src='src/shared/images/people.png' style={{width: '22px'}}></img>
+                  <img src='src/shared/images/people.png' style={{width: '40px'}}></img>
                 </div>
                 <input type="text" readOnly="true" value={'http://pharaoh.js.org/' + this.props.projectKey} />
                 <button className="btn" data-clipboard-target={'http://pharaoh.js.org/' + this.props.projectKey}>
@@ -38,4 +44,3 @@ const Header = React.createClass({
 })
 
 export default Header
-
