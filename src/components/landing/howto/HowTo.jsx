d3a9bd803186e1e6f7de4b52679bc8d93316d55c
J. Tanner Porter <jtporter9@gmail.com>
2016-01-21 00:35:40 -0700

    some text added to the landing page

--- src/components/landing/howto/HowTo.jsx
+++ src/components/landing/howto/HowTo.jsx
@@ -14,13 +14,14 @@ const HowTo = React.createClass({
           <big>About:</big>
           <p>
             We're a group of students at <a className="devmtn" href="http://devmounta.in">DevMountain</a>, building just about the neatest instructor tool you've ever seen. Imagine a lecture with no JSBin or JSFiddle, with no opening the Developer Tools in your browser just to show something in the console, with no rushed projects thrown together and then sent out on Slack. Imagine a read-only app that would allow students to see your code in realtime, as you make changes, and see everything you `import` or `require`, and check out the files you're pulling from.
-              </p><p>Well, that's what we imagined, because right now there's a ton of tooling around Javascript development, but not nearly enough tools for teaching it. We're starting with a React-based student app and a package for one of the most popular editors on the market. We hope to build this into something pretty awesome, because no one should have to use JSBin, Github, and Slack, just to get through one lecture.
+          </p><p>Well, that's what we imagined, because right now there's a ton of tooling around Javascript development, but not nearly enough tools for teaching it. We're starting with a React-based student app and a package for one of the most popular editors on the market. We hope to build this into something pretty awesome, because no one should have to use JSBin, Github, and Slack, just to get through one lecture.
           </p>
           <big>Usage:</big>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sollicitudin, mi in malesuada molestie.
-              </p><p>
-            Sed ullamcorper magna tellus, ut aliquet orci molestie vel. Quisque tempor tellus purus, ut eleifend eros porta sed. Vestibulum imperdiet suscipit placerat. Cras quis elit at lectus pulvinar rutrum.
+          For students you will recieve a string of text and to enter the session, enter that test into the "Enter session code:" input area, then hit "GO!".
+          </p><p>
+          For Teachers enter the name of your new session, start, then share your link.
+          Or you have to option to dowload Pharaoh desktop app to be able to store files local on your machine. Enjoy!
           </p>
           </div>
       </InlineCss>
