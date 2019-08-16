cd97be1ec03668376d0cba58f8898e9ce2029e57
Jt Porter <jtporter9@gmail.com>
2019-08-16 18:43:29 -0500

    first

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -1,21 +1,21 @@
-import React          from 'react'
-import InlineCss      from 'react-inline-css'
-import Header         from './header/Header'
-import HowTo          from './howto/HowTo'
-import Team           from './team/Team'
-import Stack          from './stack/Stack'
+import React from 'react'
+import InlineCss from 'react-inline-css'
+import Header from './header/Header'
+import HowTo from './howto/HowTo'
+import Team from './team/Team'
+import Stack from './stack/Stack'
 import GettingStarted from './gettingStarted/GettingStarted'
-import Firebase       from 'firebase'
+import Firebase from 'firebase'
 
 const stylesheet = require('!css!less!./landing.less').toString()
-const BASEURL    = 'https://pharaohjs.firebaseio.com/session/'
+const BASEURL = 'https://pharaohjs.firebaseio.com/session/'
 
 const Landing = React.createClass({
   initFirebase(projectName = 'Project Name') {
     let ref = new Firebase(BASEURL)
     let projectRef = ref.push({
-      projectName : projectName,
-      default: {fileName: 'gettingStarted.js', key: 'default'}
+      projectName: projectName,
+      default: { fileName: 'gettingStarted.js', key: 'default' }
     })
     return projectRef.key()
   },
@@ -23,17 +23,17 @@ const Landing = React.createClass({
     let key = this.initFirebase(projectName)
     this.props.history.pushState(null, '/app/w/' + key)
   },
-  render () {
+  render() {
     return (
       <InlineCss componentName="Landing" stylesheet={stylesheet}>
-        <div className="container">
-          <div className="header">
-            <Header />
+        <div>
+          <Header />
+          <div className="container">
+            <HowTo />
+            <GettingStarted startSession={this.startSession} />
+            <Team />
+            <Stack />
           </div>
-          <HowTo />
-          <GettingStarted startSession={this.startSession}/>
-          <Team />
-          <Stack />
         </div>
       </InlineCss>
     )
--- src/components/landing/header/Header.jsx
+++ src/components/landing/header/Header.jsx
@@ -1,19 +1,19 @@
-import React                from 'react'
-import InlineCss            from "react-inline-css"
-import {ReactRouter, Link}  from 'react-router'
+import React from 'react'
+import InlineCss from "react-inline-css"
+import { ReactRouter, Link } from 'react-router'
 const stylesheet = require('!css!less!./header.less').toString()
 
 const Header = React.createClass({
-  getInitialState () {
-    return {project: ''}
+  getInitialState() {
+    return { project: '' }
   },
-  handleProjectEntry (e) {
-    this.setState({project: e.target.value})
+  handleProjectEntry(e) {
+    this.setState({ project: e.target.value })
   },
   render() {
     return (
       <InlineCss componentName="Header" stylesheet={stylesheet}>
-        <big>Pharaoh</big>
+        {/* <big>Pharaoh</big>
         <img src="images/pharaoh.png" style={{
           width: '50px',
           position: 'relative',
@@ -28,8 +28,41 @@ const Header = React.createClass({
             ></input>
           </span>
           <Link className="link" to={'/app/r/'+this.state.project}>Go!</Link>
+        </div> */}
+        <div className="row justify-content-between align-items-center">
+          <div className="col-sm">
+            <h2>Pharaoh</h2>
+            <img src="images/pharaoh.png" style={{
+              width: '50px',
+              verticalAlign: 'bottom'
+            }}></img>
+          </div>
+          <div className="col-sm-5 text-right">
+            {/*Join Existing Session:
+            <input
+                type="text"
+                value={this.state.project}
+                onChange={this.handleProjectEntry}
+              ></input> */}
+            <div className="input-group">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="inputGroup-sizing-default">
+                  Join Existing Session:
+                  </span>
+              </div>
+              <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default"></input>
+              <div className="input-group-append">
+                <Link className="link" to={'/app/r/' + this.state.project}>
+                  <button className="btn btn-outline-secondary" type="button">
+                    Go!
+                </button>
+                </Link>
+              </div>
+            </div>
+
+          </div>
         </div>
-      </InlineCss>
+      </InlineCss >
     )
   }
 })
